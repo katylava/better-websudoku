94105db7eba9dde9b5d618e201ddccfc89b9c6cd
katy lavallee <katylava@gmail.com>
2011-08-23 23:43:22 -0500

    highlight current row, column, and block. remove notes as numbers are entered.

--- js/enhancements.js
+++ js/enhancements.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+
   var lastFocus = false
   $("td > input")
     .focus(function(ev){
@@ -18,6 +19,7 @@ $(document).ready(function(){
       else if ($(this).hasClass('multi')) {
         $(this).removeClass('multi')
       }
+      removeNotes($(this).attr('id'))
     })
 
   $('form > p > input[type="submit"]').click(function(ev){
@@ -37,6 +39,49 @@ $(document).ready(function(){
 
   function refreshStyles() {
     $('td > input:not([readonly])').trigger('keyup')
+  }
+
+  function removeNotes(id) {
+    $('.matchable').removeClass('matchable')
+
+    var tmp
+    var blocks = {c0:[1,2],c1:[0,2],c2:[0,1],c3:[4,5],c4:[3,5],c5:[3,4],c6:[7,8],c7:[6,8],c8:[6,7]}
+    var scopes = [0,1,2,3,4,5,6,7,8]
+    var col = id.split('')[1]
+    var row = id.split('')[2]
+    var rowBlock = blocks['c'+row]
+    var colBlock = blocks['c'+col]
+    var val = $('#'+id).val()
+    var ids = []
+
+    for (var i in scopes) {
+      tmp = 'c'+i+row
+      ids.push(tmp)
+      tmp = 'c'+col+i
+      if (ids.indexOf(tmp) < 0) ids.push(tmp)
+    }
+    for (var ci in colBlock) {
+      for (var ri in rowBlock) {
+        tmp = 'c'+colBlock[ci]+rowBlock[ri]
+        if (ids.indexOf(tmp) < 0) ids.push(tmp)
+      }
+    }
+
+    $.each(ids, function(i,v){
+      $('#'+v).addClass('matchable')
+    })
+    $('.matchable.multi').each(function(){
+      updateMultiVal(this, val)
+    })
+
+
+  }
+
+
+  function updateMultiVal(el, num) {
+    var val = $(el).val()
+    newVal = newVal.replace(num, '')
+    $(el).val(newVal)
   }
 
 })
