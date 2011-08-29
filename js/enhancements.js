94326dda9169ccb3feb4e600e2c9ee5410afb6a6
katy lavallee <katylava@gmail.com>
2011-08-28 20:46:29 -0500

    if digit is input which is already in the box, then remove it instead

--- js/enhancements.js
+++ js/enhancements.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
 
-
+  var keycodes = { k49: '1', k50: '2', k51: '3', k52: '4', k53: '5', k54: '6', k55: '7', k56: '8', k57: '9' }
   var lastFocus = false
+
   $("td > input")
     .focus(function(ev){
       highlightSame($(this).val())
@@ -13,15 +14,23 @@ $(document).ready(function(){
       lastFocus = $(this)
     })
     .keyup(function(ev){
-      highlightSame($(this).val())
-      if ($(this).val().length > 1) {
+      var val = $(this).val()
+      var chr = keycodes['k'+ev.which]
+
+      if (val.indexOf(chr) != val.lastIndexOf(chr)) {
+        $(this).attr('value', strReplaceAll(val, chr, ''))
+      }
+      highlightSame(val)
+
+      if (val.length > 1) {
         $(this).addClass('multi')
       } else {
         $(this).removeClass('multi')
       }
     }).change(function(ev){
-      if ($(this).val().length == 1) removeNotes(this)
-      if ($(this).hasClass('multi')) $(this).attr('value', $(this).val())
+      var val = $(this).val()
+      if (val.length == 1) removeNotes(this)
+      if ($(this).hasClass('multi')) $(this).attr('value', val)
     })
 
   $('form > p > input[type="submit"]').click(function(ev){
@@ -87,6 +96,11 @@ $(document).ready(function(){
       if (newVal.length == 1) $(this).removeClass('multi').trigger('change')
     })
     highlightSame(rep)
+  }
+
+  function strReplaceAll(str, find, repl) {
+    var reg = new RegExp(find, 'g')
+    return str.replace(reg, repl)
   }
 
 })
